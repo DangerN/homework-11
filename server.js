1a2cda3a5fad40bd2d38c423d8fe50fdca7e1895
Crystal Danger <cdneal091@gmail.com>
2020-03-16 22:04:44 -0400

    stuff

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
 
   user: "root",
 
-  password: "Carolina1984$$",
+  password: "password",
   database: "company_db"
 });
 
@@ -59,7 +59,7 @@ function addDepartment() {
         function(err) {
           if (err) throw err;
           console.log("Your department was created successfully!");
-        //   start();
+          start();
         }
       );
     });
@@ -68,53 +68,81 @@ function addDepartment() {
 // function to add employee into the table
 function addEmployee() {
     // prompt for info about waht he wants to add (department, role, or employee)
-    inquirer
-      .prompt([
-        {
-          name: "employeeFirstName",
-          type: "input",
-          message: "What is the first name of the employee you would like to add?"
-        },
-        {
-            name: "employeeLastName",
-            type: "input",
-            message: "What is the last name of the employee you would like to add?"
-          },
-          {
-            name: "employeeRoleId",
-            type: "input",
-            message: "What is the role Id of this employee?"
-          },
-          {
-            name: "employeeManagerId",
-            type: "input",
-            message: "What is the manager Id of this employee?"
-          }
 
-      ])
-      .then(function(answer) {
-        // when finished prompting, insert a new employee into the db with that info
-        connection.query(
-          "INSERT INTO employee SET ?",
-          {
-            first_name: answer.employeeFirstName,
-            last_name: answer.employeeLastName,
-            role_id: answer.employeeRoleId,
-            manager_idclear: answer.employeeManagerId,
-          },
-          function(err) {
-            if (err) throw err;
-            console.log("Your employee was added successfully!");
-          //   start();
-          }
-        );
+    let roles = []
+    let employees = []
+
+    connection.query('select * from role', function (err, res) {
+      res.forEach((item) => {
+        roles.push({name: item.name, value: item.id})
       });
+      connection.query('select * from employee', function (err, res) {
+        res.forEach((item) => {
+          employees.push({name: `${item.first_name} ${item.last_name}`, value: item.id})
+        });
+        employees.push({name: 'none', value: null})
+        inquirer
+        .prompt([
+          {
+            name: "employeeFirstName",
+            type: "input",
+            message: "What is the first name of the employee you would like to add?"
+          },
+          {
+              name: "employeeLastName",
+              type: "input",
+              message: "What is the last name of the employee you would like to add?"
+            },
+            {
+              name: "employeeRoleId",
+              type: "list",
+              message: "What is the role of this employee?",
+              choices: roles
+            },
+            {
+              name: "employeeManagerId",
+              type: "list",
+              message: "What is the manager of this employee?",
+              choices: employees
+            }
+
+        ])
+        .then(function(answer) {
+          // when finished prompting, insert a new employee into the db with that info
+          connection.query(
+            "INSERT INTO employee SET ?",
+            {
+              first_name: answer.employeeFirstName,
+              last_name: answer.employeeLastName,
+              role_id: answer.employeeRoleId,
+              manager_id: answer.employeeManagerId,
+            },
+            function(err) {
+              if (err) throw err;
+              console.log("Your employee was added successfully!");
+              start()
+            }
+          );
+        });
+      })
+    })
+
+
   }
 
   // function to add role into the table
   function addRole() {
       // prompt for info about waht he wants to add (department, role, or employee)
-      inquirer
+
+      let departments = []
+
+      connection.query("select * from department", function (err, res) {
+        console.log(res);
+        res.forEach((item) => {
+          departments.push({name: item.name, value: item.id})
+        });
+
+        inquirer
         .prompt([
           {
             name: "roleTitle",
@@ -122,30 +150,33 @@ function addEmployee() {
             message: "What title would you like to add?"
           },
           {
-              name: "salary",
-              type: "input",
-              message: "What is the salary for this role?"
-            },
-            {
-              name: "departmentId",
-              type: "input",
-              message: "What is the departmentId of this role?"
-            }
+            name: "salary",
+            type: "input",
+            message: "What is the salary for this role?"
+          },
+          {
+            name: "departmentId",
+            type: "list",
+            message: "What is the department of this role?",
+            choices: departments
+          }
         ])
         .then(function(answer) {
           // when finished prompting, insert a new role into the db with that info
+          console.log(answer);
           connection.query(
-            "INSERT INTO employee SET ?",
+            "INSERT INTO role SET ?",
             {
               title: answer.roleTitle,
               salary: answer.salary,
               department_id: answer.departmentId,
-                      },
+            },
             function(err) {
               if (err) throw err;
               console.log("Your role was added successfully!");
-            //   start();
+                start();
             }
           );
         });
+      })
     }
